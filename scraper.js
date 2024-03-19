a3f76ec0806635148c1993936ab14974573954e8
Marcod <daidone.marco38280@gmail.com>
2024-03-19 14:20:54 +0100

    slice scraper

--- scraper.js
+++ scraper.js
@@ -34,7 +34,7 @@ export async function scrapeAndSave() {
 
     // Sélectionner les 10 premiers événements
     console.log("Sélection des 10 premiers événements...");
-    const events = $('tr[itemtype="http://schema.org/Event"]').slice(0, 50);
+    const events = $('tr[itemtype="http://schema.org/Event"]').slice(0, 90);
 
     // Parcourir chaque événement
    for (let i = 0; i < events.length; i++) {
