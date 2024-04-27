00567483c985dbad4540bc5f902333f69f766c67
Marcod <daidone.marco38280@gmail.com>
2024-04-27 10:36:49 +0200

    fixe setInterval

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { scrapeAndSave } from './scraper.js';
 import { Event } from './models/events.js';
 import cors from 'cors';
 import events from './routes/events.js';
@@ -9,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Appeler la fonction scrapeAndSave toutes les semaines
-// const scrapeInterval = 6 * 24 * 60 * 60 * 1000; // 6 jours en millisecondes
-// setInterval(scrapeAndSave, scrapeInterval);
+const scrapeInterval = 6 * 24 * 60 * 60 * 1000; // 6 jours en millisecondes
+setInterval(scrapeAndSave, scrapeInterval);
 
 // Middleware
 app.use(cors());
